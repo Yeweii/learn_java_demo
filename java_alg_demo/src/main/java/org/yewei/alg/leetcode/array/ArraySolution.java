5f10df29dfb286e2d4bf3659366a3be0226759c1
yewei <yeweibeihua@gmail.com>
2025-04-18 14:41:45 +0800

    fix: update

--- java_alg_demo/src/main/java/org/yewei/alg/leetcode/array/ArraySolution.java
+++ java_alg_demo/src/main/java/org/yewei/alg/leetcode/array/ArraySolution.java
@@ -10,6 +10,54 @@ public class ArraySolution {
      *
      */
 
+    /**
+     * 使用冒泡排序算法对整数数组进行排序。
+     *
+     * @param array 待排序的整数数组。
+     */
+    public void bubbleSort(int[] array) {
+        // 外层循环控制遍历的轮数
+        for (int i = 0; i < array.length; i++) {
+            // 内层循环进行相邻元素的比较和交换
+            for (int j = 0; j < array.length - i - 1; j++) {
+                // 如果前一个元素大于后一个元素，则交换它们的位置
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+
+    /**
+     * 插入排序
+     * @param array
+     */
+    public int[] insertSort(int[] array) {
+
+
+
+
+
+        return array;
+    }
+
+
+    public static void main(String[] args) {
+        int[] array = {5, 2, 9, 1, 5, 6};
+        ArraySolution solution = new ArraySolution();
+        solution.bubbleSort(array);
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+    }
+
+
+
+
+
 
 
 }
